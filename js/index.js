67f43e5b952d9a2cf0e7431a927ca09b8e2548d5
Evelyn <yalleyevelyn@gmail.com>
2024-06-12 10:40:40 -0700

    updated

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('storebtn')
+btn.onclick = function () {
+    alert('Get a Sparklean Gift Card Today');}
+
+    const btn1 = document.getElementById('contactbtn')
+    btn1.onclick = function () {
+        alert('Free Consultation');}
+
+
+
+
+        
